5eda0d8b85582cdf9fbf05678c18a5f43ae00569
gurjap <gurjaph@my.yorku.ca>
2024-01-26 17:57:44 -0500

    change dir

--- src/aiv/screens/uploader/uploader.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-uploader',
-  templateUrl: './uploader.component.html',
-  styleUrls: ['./uploader.component.scss'],
-})
-export class UploaderComponent {}
